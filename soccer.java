97d46b8544f0d1bd9335632661371b4f8d5577bc
Grant <grant@4sitestudios.com>
2018-08-01 11:47:44 -0400

    Added soccer java file

---
+++ soccer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class socccer{
+	 public String player;
+	public int attempts; 
+	public String result; 
+	public String score; 
+		public String getPlayer() {
+			Scanner 
+		}
+}
