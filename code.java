14e683af1552c56d5cd5cf4a16bd80c41dee239d
Grant <grant@4sitestudios.com>
2018-08-01 11:14:56 -0400

    Changed the java file

---
+++ code.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class code { 
+	public static void main(String args[]) {
+		System.out.print("Enter your player's name:"); 
+		System.out.print(basketball.getrules()); 
+	}
+}
