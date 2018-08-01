9c84b850c080e52a63515a46b0536ce3a7c34bca
Grant <grant@4sitestudios.com>
2018-08-01 11:16:52 -0400

    I made some more edits to the code java file

--- code.java
+++ code.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class code { 
 	public static void main(String args[]) {
 		System.out.print("Enter your player's name:"); 
+		soccer.getPlayer(); 
+		System.out.print(soccer.getAttempts()); 
 		System.out.print(basketball.getrules()); 
 	}
 }
